44a83f43c2275731099b3999f34566e98c73f4b1
cluracan1 <cluracan1@gmail.com>
2024-11-06 21:31:51 +0000

    titleScreen initial commit

---
+++ client/titleScreen.mjs
@@ -0,0 +1,26 @@
+export default function insertTitleContent() {
+  const headerContent = document.getElementById("header-content");
+  const leftContent = document.getElementById("left-content");
+  const rightContent = document.getElementById("right-content");
+  const footerContent = document.getElementById("footer-content");
+  for (const contentDiv of [
+    headerContent,
+    leftContent,
+    rightContent,
+    footerContent,
+  ]) {
+    while (contentDiv.firstChild) {
+      contentDiv.removeChild(contentDiv.firstChild);
+    }
+  }
+
+  headerContent.textContent = "BattleShips";
+  leftContent.textContent = "LeftPic";
+  rightContent.textContent = "rightPic";
+  const startBtn = document.createElement("button");
+  startBtn.textContent = "START";
+  startBtn.addEventListener("click", (e) => {
+    console.log("start");
+  });
+  footerContent.appendChild(startBtn);
+}
